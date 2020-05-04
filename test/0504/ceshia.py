f96465c62bfc8c49cf0ee092c1a4457c2f8674b2
chenyuan23 <572883431@qq.com>
2020-05-04 16:17:17 +0800

    介添加绍家里人的类和方法

--- test/0504/ceshia.py
+++ test/0504/ceshia.py
@@ -1,3 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Author: ChenYuan
 # @Date  : 2020/5/4 14:54
+class Test():
+    def __init__(self,age,name):
+        self.age = age
+        self.name = name
+
+    def des(self):
+        print('你的儿子多大年纪了？{}岁'.format(self.age))
+
+t = Test(18,'dada')
+t.des()
